560582c63717f20ee082f7c62d3cb06d953a9a9a
cameron turner <17177161+cturner8@users.noreply.github.com>
2023-09-26 15:27:23 +0100

    add initial logic to fetch chats on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
-"use client";
+"use server";
 
-import { useAuthUser } from "@/hooks/useAuthUser";
+import type { Database } from "@/lib/database.types";
+import { supabase } from "@/lib/supabase/server";
 
-export default function Home() {
-  useAuthUser();
-  return <main>My Chats</main>;
+type Chat = Database["public"]["Tables"]["Chats"]["Row"];
+
+export default async function Home() {
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  const { user } = session ?? {};
+
+  let chats: Chat[] = [];
+
+  if (user) {
+    const { data } = await supabase
+      .from("Chats")
+      .select("*, ChatMembers(id)")
+      .eq("ChatMembers.user_id", user.id);
+
+    chats = data ?? [];
+  }
+
+  return (
+    <main>
+      <h1 className="mb-4">My Chats</h1>
+      <ul>
+        {chats.map((chat) => (
+          <li key={chat.id}>{chat.name}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }
--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,4 +1,6 @@
 import type { Database } from "@/lib/database.types";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-export const supabase = createServerComponentClient<Database>({ cookies });
+export const supabase = createServerComponentClient<Database>({
+  cookies,
+});
